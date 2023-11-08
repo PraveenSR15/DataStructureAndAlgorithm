cf6c3de3ba361626556def46a3a71a9e851f0ea7
prave <prave@SR>
2023-11-08 19:19:36 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿class program
+{
+    public static void Main(String[] args)
+    {
+        AddTwoNumbers addTwo = new AddTwoNumbers();
+        var node1 = new ListNode(2,new ListNode(4,new ListNode(3)));
+        var node2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+        var res = addTwo.Solution(node1,node2);
+    }
+    
+}
