662f1ae081e3f360df4029bc7f6f6ace092ea7e5
Praveen S R <praveentiger1997@gmail.com>
2023-11-15 00:27:18 +0530

    Stairs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using AddTwoNumbersRecursion;
 using LeetCode.IndexOfString_String_Loop;
 using LeetCode.RemoveDuplicates_Array_Loop;
+using LeetCode.Stairs_Integer_Fibonacci;
 using LeetCode.TwoSum_Array_Loop;
 using MultiplyStrings;
 
@@ -16,14 +17,17 @@ class program
         //MultiplyTwoStrings multiply = new MultiplyTwoStrings();
         //multiply.Multiply("6913259244", "71103343");
 
-        RemoveDuplicateNumbers removeDuplicateNumbers = new RemoveDuplicateNumbers();
-        removeDuplicateNumbers.RemoveDuplicates(new int[] { 1, 1, 2, 3, 5, 5, 5, 6 });
+        //RemoveDuplicateNumbers removeDuplicateNumbers = new RemoveDuplicateNumbers();
+        //removeDuplicateNumbers.RemoveDuplicates(new int[] { 1, 1, 2, 3, 5, 5, 5, 6 });
 
-        TwoSum twoSum = new TwoSum();
-        var res = twoSum.TwoSumSolution(new int[] { 2, 7, 11, 15 }, 9);
+        //TwoSum twoSum = new TwoSum();
+        //var res = twoSum.TwoSumSolution(new int[] { 2, 7, 11, 15 }, 9);
 
-        IndexOfString index = new IndexOfString();
-        index.StrStr("mississippi", "issipy");
+        //IndexOfString index = new IndexOfString();
+        //index.StrStr("mississippi", "issipy");
+
+        Stairs stairs = new Stairs();
+        var res = stairs.ClimbStairs(6);
     }
     
 }
---
+++ Stairs_Integer_Fibonacci/Stairs.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Stairs_Integer_Fibonacci
+{
+    public class Stairs
+    {
+        public int ClimbStairs(int n)
+        {
+            int previous = 1;
+            int current = 2;
+            int res = 0;
+            for(int i=3;i<=n;i++)
+            {
+                res = current + previous;
+                previous = current;
+                current = res;
+            }
+            
+            return res;
+        }
+
+        public int ClimbStairsOptimal(int n)
+        {
+            Dictionary<int, int> memo = new Dictionary<int, int>();
+            return climbStairsOptimal(n, memo);
+        }
+        private int climbStairsOptimal(int n, Dictionary<int, int> memo)
+        {
+            if (n == 0 || n == 1)
+            {
+                return 1;
+            }
+            if (!memo.ContainsKey(n))
+            {
+                memo.Add(n, climbStairsOptimal(n - 1, memo) + climbStairsOptimal(n - 2, memo));
+            }
+            return memo[n];
+        }
+    }
+}
