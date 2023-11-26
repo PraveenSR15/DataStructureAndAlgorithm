f14356534dbd83aa2bae8455a42ba49644069de7
Praveen S R <praveentiger1997@gmail.com>
2023-11-27 01:09:56 +0530

    Changes

---
+++ Permutation_Array_Loop/PermutationArray.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Permutation_Array_Loop
+{
+    public class PermutationArray
+    {
+        public int[] BuildArray(int[] nums)
+        {
+            int[] res = new int[nums.Length];
+            for(int i = 0; i < nums.Length; i++) 
+            {
+                res[i] = nums[nums[i]];
+            }
+            return res;
+    }
+    }
+}
